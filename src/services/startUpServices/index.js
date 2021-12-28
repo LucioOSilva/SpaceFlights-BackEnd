a1ec2d84b876434ff8d146e144429459a7a28ebb
LucioOSilva <lucioosilva00@gmail.com>
2021-12-28 20:52:57 -0300

    Add text explaining why node-cron wasnt installed

--- src/services/startUpServices/index.js
+++ src/services/startUpServices/index.js
@@ -1,3 +1,9 @@
 const { seedDataBaseRoutine } = require('./articlesSeedService');
 
 module.exports = { seedDataBaseRoutine };
+
+/*
+  Caro avaliador - neste projeto optei por implementar manualmente o cron,
+  compreendo o uso e a utilidade sem contar no baixo nível de complexidade para
+  implementar a lib "node-cron", mas o fiz por diversão além de um desafio a mais.
+*/
