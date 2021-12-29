cad1a10de246bc57e7834cc5dca8822c1b0e6bba
LucioOSilva <lucioosilva00@gmail.com>
2021-12-29 17:16:43 -0300

    Creates swagger openAPI

--- src/api/app.js
+++ src/api/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const openApiSwaggerDoc = require('../services/openAPI/openApiSwaggerDoc.json');
 const router = require('../routes/router');
 const mdw = require('../middlewares');
 const startUpServices = require('../services/startUpServices');
@@ -10,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(router);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiSwaggerDoc));
 app.use(mdw.mdwError.errorMiddleware);
 
 startUpServices.seedDataBaseRoutine();
