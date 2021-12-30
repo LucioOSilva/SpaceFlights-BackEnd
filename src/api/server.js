c6b91e330b76f741fd2dc9dfe660b0d01705b515
LucioOSilva <lucioosilva00@gmail.com>
2021-12-30 14:52:06 -0300

    Changes the app message when start up

--- src/api/server.js
+++ src/api/server.js
@@ -4,4 +4,4 @@ const app = require('./app');
 const PORTFIXED = 3333;
 const PORT = process.env.PORT || PORTFIXED;
 
-app.listen(PORT, () => console.log(`conectado na porta ${PORT}`));
+app.listen(PORT, () => console.log(`connected at port: ${PORT}`));
