330b8a87d22f087497da7023978a93b8bc905b96
LucioOSilva <lucioosilva00@gmail.com>
2021-12-31 14:41:21 -0300

    Changes on model

--- src/models/articlesModel.js
+++ src/models/articlesModel.js
@@ -2,23 +2,20 @@
 const connection = require('./connection');
 
 const getOneArticle = async (objSearch) => {
-  const mongoConnection = await connection();
-  const articles = mongoConnection.then((db) => db.collection('articles').findOne(objSearch));
-  mongoConnection.then((db) => db.close());
+  const articles = await connection()
+    .then((db) => db.collection('articles').findOne(objSearch));
   return articles;
 };
 
 const getAllArticles = async () => {
-  const mongoConnection = await connection();
-  const articles = mongoConnection.then((db) => db.collection('articles').find().toArray());
-  mongoConnection.then((db) => db.close());
+  const articles = await connection()
+    .then((db) => db.collection('articles').find().toArray());
   return articles;
 };
 
 const getAllArticlesCount = async () => {
-  const mongoConnection = await connection();
-  const articles = mongoConnection.then((db) => db.collection('articles').count());
-  mongoConnection.then((db) => db.close());
+  const articles = await connection()
+    .then((db) => db.collection('articles').count());
   return articles;
 };
 
@@ -35,53 +32,45 @@ const getArticlesByPage = async (page) => {
 };
 
 const upsertManyArticles = async (arrayOfArticles) => {
-  const mongoConnection = await connection();
-  const articles = mongoConnection.then((db) => {
-    arrayOfArticles.forEach((document) => {
-      db.collection('articles').updateMany({ id: document.id }, { $set: document }, { upsert: true });
+  const articles = await connection()
+    .then((db) => {
+      arrayOfArticles.forEach((document) => {
+        db.collection('articles').updateMany({ id: document.id }, { $set: document }, { upsert: true });
+      });
     });
-  });
-  mongoConnection.then((db) => db.close());
   return articles;
 };
 
 const postOneArticle = async (articleDTO) => {
-  const mongoConnection = await connection();
-  const articles = mongoConnection
+  const articles = await connection()
     .then((db) => db.collection('articles').insertOne(articleDTO, { writeConcern: { w: 'majority', wtimeout: 500 } }));
-  mongoConnection.then((db) => db.close());
   return articles;
 };
 
 const putManyArticles = async (arrayOfArticles) => {
-  const mongoConnection = await connection();
-  const articles = mongoConnection.then((db) => db.collection('articles').insertMany(arrayOfArticles));
-  mongoConnection.then((db) => db.close());
+  const articles = await connection()
+    .then((db) => db.collection('articles').insertMany(arrayOfArticles));
   return articles;
 };
 
 const updateOneArticle = async (id, props) => {
-  const mongoConnection = await connection();
-  const articles = mongoConnection
+  const articles = await connection()
     .then((db) => db.collection('articles').updateOne({ id }, { $set: { ...props } }, { upsert: true }))
     .then(() => ({ id }));
-  mongoConnection.then((db) => db.close());
   return articles;
 };
 
 const deleteAllData = async () => {
-  const mongoConnection = await connection();
-  const articles = mongoConnection.then((db) => db.dropDatabase());
-  mongoConnection.then((db) => db.close());
+  const articles = await connection()
+    .then((db) => db.dropDatabase());
   return articles;
 };
 
 const deleteOneArticle = async (id) => {
   const article = await getOneArticle({ id });
   if (!article) return false;
-  const mongoConnection = await connection();
-  const articles = mongoConnection.then((db) => db.collection('articles').deleteOne({ id: article.id }));
-  mongoConnection.then((db) => db.close());
+  const articles = await connection()
+    .then((db) => db.collection('articles').deleteOne({ id: article.id }));
   return articles;
 };
 
